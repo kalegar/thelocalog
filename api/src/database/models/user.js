a57ba60dde5b18c760da263a89bc40e499cd24d2
kalegar <sanjy33@hotmail.com>
2021-03-02 20:39:06 -0700

    WIP implement users and authentication

---
+++ api/src/database/models/user.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    id: { type: DataTypes.UUID, allowNull: false, primaryKey: true, validate: { notNull: true }, defaultValue: Sequelize.UUIDV4 },
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: { type: DataTypes.STRING, allowNull: false, unique: true},
+    password: DataTypes.STRING,
+    permissionLevel: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
--- api/src/server.js
+++ api/src/server.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import merchantRoutes from './routes/merchants.routes.js';
 import categoryRoutes from './routes/categories.routes.js';
+import userRoutes from './routes/users.routes.js';
 
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(express.static(path.join(__dirname, `${baseURL}merchantapp/dist`)));
 
 app.use('/api/merchants',merchantRoutes);
 app.use('/api/categories',categoryRoutes);
+app.use('/api/users',userRoutes);
 
 app.get('/', (req,res) => {
    res.sendFile(path.join(__dirname, `${baseURL}merchantapp/dist/index.html`));
